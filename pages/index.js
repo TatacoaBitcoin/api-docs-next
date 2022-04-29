2c8f1e1fc08f84e1ee5e6990faa5722f9d110a22
Daniel Trujillo <danielfetrujillo@gmail.com>
2022-04-29 09:37:41 -0500

    added content

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "../styles/Home.module.css";
@@ -24,7 +26,7 @@ export default function Home() {
           <h1>Tatacoa Bitcoin API</h1>
         </header>
 
-        <div className={styles.container}>
+        <Container className="p-5">
           <section className={styles.section}>
             <h2>Autenticación</h2>
             <p>
@@ -34,7 +36,231 @@ export default function Home() {
               autenticación recibido.
             </p>
           </section>
-        </div>
+
+          <section className={styles.section}>
+            <h2>Invoices</h2>
+
+            <h3>Creación</h3>
+            <Table bordered size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>POST</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/invoices</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h4>Payload</h4>
+            <pre>
+              <code>{`
+  {
+    "amount": 250,
+    "currency": "COP",
+    "description": "tatacoa bitcoin invoice",
+    "webhook": "https://webhook.com"
+  }
+          `}</code>
+            </pre>
+
+            <h3>Lista</h3>
+            <Table bordered size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/invoices</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h3>Detalles</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/invoices/:invoiceId</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </section>
+
+          <section className={styles.section}>
+            <h2>Payments</h2>
+
+            <h3>Creación</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>POST</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/payments</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h4>Payload</h4>
+            <pre>
+              <code>{`
+  {
+    "invoice": "lnbc1150n1p3qhh...3pf2gpe3g2usn0qcz49gq6nqwxw"
+  }
+          `}</code>
+            </pre>
+
+            <h3>Lista</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/payments</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h3>Detalles</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/payments/:paymentId</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </section>
+
+          <section className={styles.section}>
+            <h2>Gifts</h2>
+
+            <h3>Creación</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>POST</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/gifts</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h4>Payload</h4>
+            <pre>
+              <code>{`
+  {
+    "amount": 100,
+    "currency": "COP",
+    "description": "tatacoa bitcoin gift"
+  }
+          `}</code>
+            </pre>
+
+            <h3>Lista</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/gifts</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h3>Detalles</h3>
+            <Table bordered responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Método</th>
+                  <th>Ruta</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className={styles.code}>GET</span>
+                  </td>
+                  <td>
+                    <span className={styles.code}>/gits/:giftId</span>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </section>
+        </Container>
       </main>
 
       <footer className={styles.footer}>Tatacoa Bitcoin 2022</footer>
